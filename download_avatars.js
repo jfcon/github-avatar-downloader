11fc1da5b31beefb55abcdd8377fc18c0be9adbb
John <connollyjohnf@gmail.com>
2019-02-12 03:26:12 +0000

    printing avatar_urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,27 +1,25 @@
 var request = require("request");
-var secrets = require("secrets");
+var secrets = require("./secrets.js");
+var fs = require("fs");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    url:
-      "https://api.github.com/repos/" +
-      repoOwner +
-      "/" +
-      repoName +
-      "/contributors",
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      "User-Agent": "request"
+      "User-Agent": "request",
+      Authorization: "token c564dc38e37f057b6849e326c9aff58ed560d392"
     }
   };
-
   request(options, function(err, res, body) {
     cb(err, body);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  var list = JSON.parse(result);
+  for (i = 0; i < list.length; i++) {
+    console.log(list[i].avatar_url);
+  }
 });
